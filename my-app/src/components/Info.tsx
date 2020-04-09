e5f111af1890cb2c72e91ff9f40c629f9adf4672
pedroribeiro89 <pedroribeiro89@hotmail.com>
2020-04-09 10:44:41 -0300

    finalizando projeto

--- my-app/src/components/Info.tsx
+++ my-app/src/components/Info.tsx
@@ -7,7 +7,7 @@ function Info() {
             <h1>Sobre o Dashboard</h1>
             <p style={InfoStyle.text}>
                 Os dados deste dashboard são
-                da <a style={InfoStyle.link} href='https://github.com/CSSEGISandData/COVID-19'>Johns Hopkins CSSE</a> que sao fornecidos pela api <a style={InfoStyle.link} href='https://covid19api.com/'>covid19api</a> Tem como objetivo comparar a evolução dos casos de Corona vírus(Confirmados, Mortes e Recuperados) entre países.
+                da <a style={InfoStyle.link} target="_blank" href='https://github.com/CSSEGISandData/COVID-19'>Johns Hopkins CSSE</a> que sao fornecidos pela api <a style={InfoStyle.link} target="_blank" href='https://covid19api.com/'>covid19api</a> Tem como objetivo comparar a evolução dos casos de Corona vírus(Confirmados, Mortes e Recuperados) entre países.
             </p>
         </article>
     );
